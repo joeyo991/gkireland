b4ac75be22e457562f3b2b57911e590c2122fa61
joeyo991 <joeyosull@gmail.com>
2022-09-13 13:36:56 +0000

    Created the checkout app and the order model

---
+++ checkout/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    address_line1 = models.CharField(max_length=80, null=False, blank=False)
+    address_line2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=40, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    delivery_charge = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+
