c36c2bd678978b16ef893a20ce563cd5f4f5ddc3
joeyo991 <joeyosull@gmail.com>
2022-09-13 14:06:24 +0000

    Finished models and migrated

--- checkout/models.py
+++ checkout/models.py
@@ -1,7 +1,9 @@
 import uuid
+
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+
 from products.models import Product
 
 
@@ -21,6 +23,29 @@ class Order(models.Model):
     delivery_charge = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
+    def _generate_order_number(self):
+        """ Generates a unique order number with UUID """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """ Update total each time an item is added """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_charge = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_charge = 0
+        self.grand_total = self.order_total + self.delivery_charge
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """ Override original save to set the order number """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
@@ -28,3 +53,14 @@ class OrderLineItem(models.Model):
     product_size = models.CharField(max_length=1, null=True, blank=True)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override original save to set the lineitem total
+        and update the order total
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} from order {self.order.order_number}'
