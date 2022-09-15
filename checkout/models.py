a58a3c6452b48a06a2faf23661ba8d961d6d8bcb
joeyo991 <joeyosull@gmail.com>
2022-09-15 14:34:55 +0000

    Created the checkout_success template

--- checkout/models.py
+++ checkout/models.py
@@ -29,7 +29,7 @@ class Order(models.Model):
 
     def update_total(self):
         """ Update total each time an item is added """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_charge = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
