3409e4d4881a3fc831ca5b208f3dcb2a62ff12d3
joeyo991 <joeyosull@gmail.com>
2022-09-15 14:04:45 +0000

    Created the checkout_success view and url

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
@@ -7,11 +7,11 @@ import stripe
 from basket.contexts import basket_contents
 from products.models import Product
 from .forms import OrderForm
-from .models import OrderLineItem
+from .models import Order, OrderLineItem
 
 
 def checkout(request):
-    """ Allows customer to checkout """
+    """ Handles checkouts """
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
@@ -90,6 +90,25 @@ def checkout(request):
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """ Handle successful checkouts """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+            email will be sent to {order.email}.')
+
+    if 'basket' in request.session:
+        del request.session['basket']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
     }
 
     return render(request, template, context)
