5eee1babfd1fe6200758a1acd2990351cdc4e8ad
joeyo991 <joeyosull@gmail.com>
2022-09-19 17:48:58 +0000

    Secured views by importing login_required for necessary views

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from checkout.models import Order
 from .models import UserProfile
 from .forms import UserInfoForm
-from checkout.models import Order
 
 
+@login_required
 def profile(request):
     """ Displays Users' profiles """
     profile = get_object_or_404(UserProfile, user=request.user)
