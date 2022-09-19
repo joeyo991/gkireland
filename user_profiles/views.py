551e9ee138a23807cd9dbbf49d9c7519146b6447
joeyo991 <joeyosull@gmail.com>
2022-09-19 10:32:31 +0000

    Created UserInfoForm to store the user's default information

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
+from .forms import UserInfoForm
 
 
 def profile(request):
     """ Displays Users' profiles """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    form = UserInfoForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'user_profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, template, context)
