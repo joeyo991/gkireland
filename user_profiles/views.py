c5b1f916faa66ca8c008de62b83e96e06566abe0
joeyo991 <joeyosull@gmail.com>
2022-09-19 11:02:11 +0000

    Updated views.py so the form updates the user's profile on submission

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import UserProfile
 from .forms import UserInfoForm
 
@@ -6,6 +7,12 @@ from .forms import UserInfoForm
 def profile(request):
     """ Displays Users' profiles """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserInfoForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile Information updated successfully!')
 
     form = UserInfoForm(instance=profile)
     orders = profile.orders.all()
