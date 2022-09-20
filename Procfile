4a821e0e62bbb3e38d4ef334643b7389ffbf9da7
joeyo991 <joeyosull@gmail.com>
2022-09-20 11:57:39 +0000

    added the heroku app domain to allowed hosts

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gkireland.wsgi.application
--- gkireland/settings.py
+++ gkireland/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$5j6ykf9&g@*#aica)5tb=^apeb6$#jg2fsf(84ld@fr7ni+1x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gk-ireland.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -117,12 +117,17 @@ WSGI_APPLICATION = 'gkireland.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-      'default': {
-          'ENGINE': 'django.db.backends.sqlite3',
-          'NAME': BASE_DIR / 'db.sqlite3',
-        }
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+            }
+    }
 
 
 # Password validation
