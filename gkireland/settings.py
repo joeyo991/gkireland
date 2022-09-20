6f2d4c615ec39a3769ac61a57ee73985fc09d520
joeyo991 <joeyosull@gmail.com>
2022-09-20 16:28:41 +0000

    Removed cache control

--- gkireland/settings.py
+++ gkireland/settings.py
@@ -171,11 +171,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    # Cache Control
-    AWS_S3_OBJECT_PARAMETERS {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
-    }
+    # # Cache Control
+    # AWS_S3_OBJECT_PARAMETERS {
+    #     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    #     'CacheControl': 'max-age=94608000',
+    # }
 
     # Bucket Configuration
     AWS_STORAGE_BUCKET_NAME = 'gk-ireland'
